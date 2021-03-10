21f0e316630c29890591202dce6b9019bed28901
kjuraszek <donkrissocorleone@gmail.com>
2021-03-10 23:10:01 +0100

    Vue clock
    Simple clock made with vue.js

--- vue-clock/src/App.vue
+++ vue-clock/src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="vue-app">
-    <p>Vue clock</p>
+    <Clock />
   </div>
 </template>
 
 <script>
 
+import Clock from './components/Clock.vue'
+
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Clock
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ vue-clock/src/components/Clock.vue
@@ -0,0 +1,237 @@
+<template>
+  <div>
+    <div class="main-clock-container">
+      <div class="lines-container">
+        <div class="shield-line line-1-container"></div>
+        <div class="shield-line line-2-container"></div>
+        <div class="shield-line line-3-container"></div>
+        <div class="shield-line line-4-container"></div>
+        <div class="shield-line line-5-container"></div>
+        <div class="shield-line line-6-container"></div>
+      </div>
+      <div class="shield-container">
+        <div class="main-clock">
+          <div 
+          class="second-hand-container" 
+          v-bind:style="{ transform: 'rotate(' + currentSecondsDegree + 'deg)' }">
+            <div class="second-hand"></div>
+          </div>
+          <div 
+          class="minute-hand-container"
+          v-bind:style="{ transform: 'rotate(' + currentMinutesDegree + 'deg)' }">
+            <div class="minute-hand"></div>
+          </div>
+          <div 
+          class="hour-hand-container"
+          v-bind:style="{ transform: 'rotate(' + currentHoursDegree + 'deg)' }">
+            <div class="hour-hand"></div>
+          </div>
+          <div class="shield-pin"></div>
+        </div>
+      </div>
+    </div>
+    <div class="time-container">
+        <p>
+          {{currentTime.hours}}<span>:</span>{{currentTime.minutes}}<span>:</span>{{currentTime.seconds}} (UTC +0)
+        </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Clock',
+  data : function(){
+    let currentDate = new Date();
+        return {
+            secondsDegree : currentDate.getSeconds() * 6,
+            minutesDegree : currentDate.getMinutes() * 6 + currentDate.getSeconds() * 6 / 60,
+            hoursDegree : currentDate.getUTCHours() % 12 * 30 + currentDate.getMinutes() * 6 / 12,
+            time : currentDate,
+            interval : false,
+            utcZone : 0
+        }
+    },
+    computed:{
+        currentSecondsDegree : {
+          get: function(){
+            return this.secondsDegree;   
+          },
+          set: function(value){
+            this.secondsDegree = value;
+          },
+        },
+        currentMinutesDegree : {
+          get: function(){
+            return this.minutesDegree;   
+          },
+          set: function(value){
+            this.minutesDegree = value;
+          },
+        },
+        currentHoursDegree : {
+          get: function(){
+            return this.hoursDegree;   
+          },
+          set: function(value){
+            this.hoursDegree = value;
+          },
+        },
+        currentTime: {
+          get: function(){
+            return {
+              hours: this.time.getHours() < 10 ? `0${this.time.getHours().toString()}` : this.time.getHours().toString(),
+              minutes: this.time.getMinutes() < 10 ? `0${this.time.getMinutes().toString()}` : this.time.getMinutes().toString(),
+              seconds: this.time.getSeconds() < 10 ? `0${this.time.getSeconds().toString()}` : this.time.getSeconds().toString()
+            }
+          },
+          set: function(date){
+            this.time = date;
+          },
+        },
+        currentZone: {
+          get: function(){
+            return this.utcZone;
+          },
+          set: function(zone){
+            this.utcZone = zone;
+          },
+        }
+    },
+    mounted : function(){
+        this.tick();     
+    },
+    beforeDestroy() {
+       clearInterval(this.interval);
+    },
+    methods : {
+        tick : function(){          
+            this.interval = setInterval(function(){
+              let date = new Date();
+
+              // set UTC hours first
+              date.setHours(date.getUTCHours() + this.utcZone);
+
+              // 360deg by 60s gives 6 degrees per second
+              this.currentSecondsDegree = date.getSeconds() * 6;
+
+              // 360deg by 60m gives 6 degrees per minute PLUS seconds proportionally
+              this.currentMinutesDegree = date.getMinutes() * 6 + date.getSeconds() * 6 / 60;
+
+              // 360deg by 12h gives 30 degrees per hour PLUS minutes proportionally
+              this.currentHoursDegree = date.getHours() % 12 * 30 + date.getMinutes() * 6 / 12;
+              this.currentTime = date;
+            }.bind(this), 0.1);
+        }
+    },
+}
+</script>
+
+<style scoped>
+.main-clock-container{
+  z-index:1;
+  width:300px;
+  height:300px;
+  margin: auto;
+  border:2px solid black;
+  border-radius: 100%;
+  position: relative;
+}
+.time-container{
+  text-align: center;
+  font-size: 24px;
+}
+.lines-container{
+  width:100%;
+  height:100%;
+  position: absolute;
+}
+.shield-container{
+  width:100%;
+  height:100%;
+  position: absolute;
+}
+.shield-line{
+  width:2%;
+  height:100%;
+  left: 49%;
+  background:black;
+  position: absolute;
+}
+.line-1-container{
+  transform:rotate(0deg);
+}
+.line-2-container{
+  transform:rotate(30deg);
+}
+.line-3-container{
+  transform:rotate(60deg);
+}
+.line-4-container{
+  transform:rotate(90deg);
+}
+.line-5-container{
+  transform:rotate(120deg);
+}
+.line-6-container{
+  transform:rotate(150deg);
+}
+.main-clock{
+  width:80%;
+  height:80%;
+  top: 10%;
+  left:10%;
+  background: #fff;
+  border-radius: 100%;
+  position: relative;
+  z-index:10;
+}
+.second-hand-container, .minute-hand-container, .hour-hand-container{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index:20;
+}
+.second-hand{
+  width:2%;
+  height:50%;
+  margin-top:0;
+  margin-left: 49%;
+  display:block;
+  background:red;
+  border-radius: 50%;
+  z-index:30;
+}
+.minute-hand{
+  width:4%;
+  height:45%;
+  margin-top:5%;
+  margin-left: 48%;
+  display:block;
+  background:black;
+  border-radius: 50%;
+  z-index:40;
+}
+.hour-hand{
+  width:4%;
+  height:30%;
+  margin-top:20%;
+  margin-left: 48%;
+  display:block;
+  background:black;
+  border-radius: 50%;
+  z-index:50;
+}
+.shield-pin{
+  position: absolute;
+  z-index:20;
+  width:6%;
+  height:6%;
+  left: 47%;
+  top: 47%;
+  display:block;
+  background:blue;
+  border-radius: 50%;
+  z-index:60;
+}
+</style>
