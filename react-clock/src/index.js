38e2df8383c9cc85e5e8b725f09d967207ff6eb9
kjuraszek <donkrissocorleone@gmail.com>
2021-03-24 23:03:01 +0100

    Added uikit to React clock

--- react-clock/src/index.js
+++ react-clock/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import 'uikit/dist/css/uikit.min.css'
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
+
+UIkit.use(Icons)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
