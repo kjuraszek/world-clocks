1e679faa19c767f5970f58a32f21a4a38bd80493
kjuraszek <donkrissocorleone@gmail.com>
2021-03-25 21:03:07 +0100

    Zone-switcher for React clock

--- react-clock/src/components/Clock.js
+++ react-clock/src/components/Clock.js
@@ -1,6 +1,37 @@
 import React from 'react';
 import './Clock.css';
+const classNames = require("classnames");
 
+//temporary solution
+const CITIES = {
+    '-12':'Baker Island',
+    '-11':'Niue',
+    '-10':'Honolulu',
+    '-9':'Anchorage',
+    '-8':'Los Angeles',
+    '-7':'Denver',
+    '-6':'Mexico City',
+    '-5':'New York',
+    '-4':'Santiago',
+    '-3':'São Paulo',
+    '-2':'Fernando de Noronha',
+    '-1':'Ittoqqortoormiit',
+    '0':'London',
+    '1':'Warsaw',
+    '2':'Cairo',
+    '3':'Moscow',
+    '4':'Dubai',
+    '5':'Karachi',
+    '6':'Dhaka',
+    '7':'Jakarta',
+    '8':'Shanghai',
+    '9':'Tokyo',
+    '10':'Sydney',
+    '11':'Nouméa',
+    '12':'Auckland',
+    '13':'Samoa',
+    '14':'Line Islands'
+    };
 
 class Clock extends React.Component{
     constructor(){
@@ -16,8 +47,12 @@ class Clock extends React.Component{
             secondsDegree: currentDate.getSeconds() * 6,
             minutesDegree: currentDate.getMinutes() * 6 + currentDate.getSeconds() * 6 / 60,
             hoursDegree : currentDate.getHours() % 12 * 30 + currentDate.getMinutes() * 6 / 12,
+            city : CITIES[randomZone.toString()],
+            animate : false,
         }
         this.clockTick = this.clockTick.bind(this);
+        this.decrementZone = this.decrementZone.bind(this);
+        this.incrementZone = this.incrementZone.bind(this);
     }
     componentDidMount(){
         let interval = setInterval(this.clockTick, 0.1);
@@ -37,10 +72,46 @@ class Clock extends React.Component{
             hoursDegree : currentDate.getHours() % 12 * 30 + currentDate.getMinutes() * 6 / 12,
         });
     }
+    decrementZone(){
+        if(this.state.zone > -12){
+            this.setState((state) => ({
+                zone : state.zone - 1,
+                city : CITIES[(state.zone - 1).toString()],
+                animate : true
+            }));
+            setTimeout(() => {
+                this.setState({ animate : false });
+            }, 500);
+        }
+    }
+    incrementZone(){
+        if(this.state.zone < 14){
+            this.setState((state) => ({
+                zone : state.zone + 1,
+                city : CITIES[(state.zone + 1).toString()],
+                animate : true
+            }));
+            setTimeout(() => {
+                this.setState({ animate : false });
+            }, 500);
+        }
+    }
     render(){
         return(
             <React.Fragment>
                 <div>
+                <div class="city-container uk-container uk-margin-auto uk-padding-small">
+                <h3 
+                className={classNames(
+                        [
+                            "uk-margin-none",
+                            "uk-text-center",
+                            this.state.animate ? "uk-animation-slide-top" : ""
+                        ]
+                    )}>
+                    {this.state.city}
+                </h3>
+                </div>
                     <div class="main-clock-parent">
                         <div class="main-clock-height-fix"></div>
                         <div class="main-clock-container">
@@ -77,11 +148,45 @@ class Clock extends React.Component{
                             </div>
                         </div>
                     </div>
-                    <div class="time-container">
-                        <p>
-                        {this.state.time} (UTC{this.state.zone >= 0 ? '+' + this.state.zone : this.state.zone})
+
+                    <div class="time-container uk-container uk-margin-auto uk-padding-small">
+                        <p class="uk-margin-none uk-text-large uk-text-center">
+                            {this.state.time}
                         </p>
                     </div>
+
+                    <div class="zone-container uk-container uk-margin-auto uk-padding-small">
+                        <div class="uk-flex uk-flex-center uk-flex-middle uk-text-center">
+                            <div class="uk-flex-none">
+                                <button 
+                                class="uk-padding-small uk-button uk-button-default"
+                                disabled={this.state.zone < -11}
+                                onClick={this.decrementZone}>
+                                    <span uk-icon="icon: chevron-left" ratio="1.5"></span>
+                                </button>
+                            </div>
+                            <div 
+                            class="uk-flex-1">
+                                <p className={classNames(
+                                    [
+                                        "uk-margin-none",
+                                        "uk-text-large",
+                                        this.state.animate ? "uk-animation-slide-bottom" : ""
+                                    ]
+                                )}>
+                                UTC{this.state.zone >= 0 ? '+' + this.state.zone : this.state.zone}
+                                </p>
+                            </div>
+                            <div class="uk-flex-none">
+                                <button 
+                                    class="uk-padding-small uk-button uk-button-default"
+                                    disabled={this.state.zone > 13}
+                                    onClick={this.incrementZone}>
+                                        <span uk-icon="icon: chevron-right" ratio="1.5"></span>
+                                </button>
+                            </div>
+                        </div>
+                        </div>
                 </div>
             </React.Fragment>
         );
